602ee2932c7526d28462d9837b1b61533e2e1339
Sergi Villanueva <sergi.villanueva@gmail.com>
2019-06-30 12:14:20 +0200

    Added thumb version for Picture

--- spec/features/admin/dashboard_spec.rb
+++ spec/features/admin/dashboard_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
+require './spec/support/wait_for_ajax.rb'
+
+RSpec.configure do |c|
+  c.include WaitForAjax
+end
 
 RSpec.describe 'Dashboard', type: :feature do
   before do
-    create_list(:picture, 25)
+    create_list(:picture, 5)
   end
 
   let(:file_path) do
@@ -22,7 +27,10 @@ RSpec.describe 'Dashboard', type: :feature do
   scenario 'User uploads file', js: true do
     visit admin_dashboard_path
     page.attach_file('picture_asset', file_path)
-    expect { click_button 'Create Picture' }.to change { Picture.count }.by(1)
+    expect do
+      click_button 'Create Picture'
+      wait_for_ajax
+    end.to change { Picture.count }.by(1)
   end
 
   scenario 'User uploads blank file', js: true do
@@ -35,7 +43,7 @@ RSpec.describe 'Dashboard', type: :feature do
     pictures_count = Picture.count
     page.attach_file('picture_asset', [file_path, other_file_path])
     click_button 'Create Picture'
+    wait_for_ajax
     expect(Picture.count).to eq(pictures_count + 2)
   end
-
 end
